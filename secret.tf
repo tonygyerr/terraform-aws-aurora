1da44427da40eb362344bc279cb4b87deeae617c
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-09-11 22:32:44 -0400

    rds

--- secret.tf
+++ secret.tf
@@ -12,5 +12,5 @@ resource "aws_secretsmanager_secret" "my_secret_user" {
 
 resource "aws_secretsmanager_secret_version" "my_secret_rds_postgres_user_map" {
   secret_id     = aws_secretsmanager_secret.my_secret_user.id
-  secret_string = aws_rds_cluster.this.master_password
+  secret_string = aws_rds_cluster.rds.master_password
 } 
