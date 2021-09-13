cbcd3c9bcda2d8d013e24377fab8337d4f2255c8
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-09-13 12:56:20 -0400

    events_subscription.tf

--- events_subscription.tf
+++ events_subscription.tf
@@ -12,6 +12,6 @@ resource "aws_db_event_subscription" "cluster_instance_event_subscription" {
   name             = "${var.cluster_identifier}-instance-event-sub"
   sns_topic        = var.sns_topic
   source_type      = "db-instance"
-  source_ids       = ["${aws_rds_cluster_instance.api_apidb_sql_rds[count.index].id}"]
+  source_ids       = ["${aws_rds_cluster_instance.rds[count.index].id}"]
   event_categories = var.cluster_instance_event_categories
 }
