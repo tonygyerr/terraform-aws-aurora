53cbada7ff453c0f7a5956f7358d5b99dbc3bb92
Tony Gyepi-Garbrah <a.gyepigarbrah@gmail.com>
2022-06-02 12:56:58 -0400

    Update outputs.tf

--- examples/outputs.tf
+++ examples/outputs.tf
@@ -18,3 +18,19 @@ output "reader_endpoint" {
 output "security_group" {
   value = "${module.aurora.security_group}"
 }
+
+output "master_username" {
+  value = "${module.aurora.master_username}"
+}
+
+output "master_password" {
+  value = "${module.aurora.master_password}"
+}
+
+output "engine" {
+  value = "${module.aurora.engine}"
+}
+
+output "port" {
+  value = "${module.aurora.port}"
+}
