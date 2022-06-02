730537fc798cd4af4d04340874083217e0e916d3
Tony Gyepi-Garbrah <a.gyepigarbrah@gmail.com>
2022-06-02 08:17:04 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,10 +1,4 @@
 provider "aws" {
-  version = "~> 2.0"
-  region  = "us-east-1"
-  profile = "default"
-}
-
-terraform {
-  backend "s3" {
-  }
+  region = var.aws_region
+  profile = var.profile
 }
