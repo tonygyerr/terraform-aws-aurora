b705474c102e8533cd7171c7a4ff3ad9c157923b
Tony Gyepi-Garbrah <a.gyepigarbrah@gmail.com>
2022-06-03 14:20:56 -0400

    Update vars.tf

--- examples/vars.tf
+++ examples/vars.tf
@@ -2,10 +2,6 @@ variable "vpc_id" {
   type = string
 }
 
-# variable "aws_region" {
-#   type = string
-# }
-
 variable "app_name" {
   type = string
   default = ""
@@ -28,7 +24,6 @@ variable "initial_db" {
 variable "private_db_subnet_ids" {
   description = "application private subnets for the vpc"
   type        = list
-  #default     = ["172.27.162.96/28","10.0.0.0/28","10.0.0.0/28"]
 }
 
 variable "extra_tags" {
@@ -147,9 +142,3 @@ variable "sns_topic" {
   type    = string
   default = ""
 }
-
-variable "engine" {
-  description = "Name of the engine"
-  default     = ""
-}
-
